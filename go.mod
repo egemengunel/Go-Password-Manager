20c5165c6359016c00cb31803d03a1ba46ddb3cb
Egemen Gunel <egemengunel@gmail.com>
2025-05-10 15:10:40 +0200

    Refactor key derivation functions and implement Argon2id hashing for master password

--- go.mod
+++ go.mod
@@ -2,9 +2,9 @@ module github.com/egemengunel/Go-Password-Manager
 
 go 1.24.2
 
-require golang.org/x/crypto v0.38.0
+require golang.org/x/crypto v0.38.0//indirect
 
 require (
 	github.com/alexedwards/argon2id v1.0.0
-	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/sys v0.33.0// indirect
 )
--- vault/kdf.go
+++ vault/kdf.go
@@ -5,14 +5,26 @@ import (
 	"log"
 
 	"github.com/alexedwards/argon2id"
-	"golang.org/x/crypto/argon2"
 )
 
-func DeriveKey(password string) []byte {
-	salt := []byte("fixed-or-random-salt-here")
-	return argon2.IDKey([]byte(password), salt, 1, 64*1024, 4, 32)
+var Parameters = &argon2id.Params{
+	Memory:      64 * 1024,
+	Iterations:  3,
+	Parallelism: 2,
+	SaltLength:  16,
+	KeyLength:   32,
 }
 
+// Hash Master Password takes users master password to enter the app and returns an encoded argon2id string
+func HashMasterPassword(password string) (encodedHash string, err error) {
+	return argon2id.CreateHash(password, Parameters)
+}
+
+func VerifyMasterPassword(password, encodedHash string) (match bool, err error) {
+	return argon2id.ComparePasswordAndHash(password, encodedHash)
+}
+
+// this is a test func to see how the package works
 func PasswordHashCheck() {
 	password := "hunter2"
 	hash, err := argon2id.CreateHash(password, argon2id.DefaultParams)
